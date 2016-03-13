f07b05c71691febf4040228e9d854ad651ce1c20
Sunil Vishwakarma <sunil.vishwakarma@gmail.com>
2016-03-12 19:06:52 -0600

    Ep2 Finished

--- src/com/sun/rest/status/V1_status.java
+++ src/com/sun/rest/status/V1_status.java
@@ -6,9 +6,18 @@ import javax.ws.rs.core.*;
 @Path("/v1/status")
 public class V1_status {
 
+	private static final String api_version = "00.01.00";
+	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getVersion(){
+		return "<p>Version: " + this.api_version + "</p>";
 	}
 }
